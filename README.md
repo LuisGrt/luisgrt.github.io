40383f32c6498719419ce87f46b734af7da08e4a
LuisGrt <LuisGermanRT@gmail.com>
2021-09-03 04:00:02 +0100

    6174

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# luisgrt.github.io
+# Basic GitHub page
 
 [View website](https://luisgrt.github.io/index.html)
---
+++ assets/scripts/_.js
@@ -0,0 +1,7 @@
+console.log(
+    "%cNOTHING TO SEE HERE",
+    "color:red;font-family:system-ui;font-size:3rem;-webkit-text-stroke: 1px black;font-weight:bold"
+  );
+  
+  console.log("https://luisgerman.dev/");
+  
